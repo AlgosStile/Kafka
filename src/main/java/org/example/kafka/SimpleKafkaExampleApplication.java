71e66bf3fef3c486f791f9e873576209b401f0a6
AlgosStile <oleg.todor@mail.ru>
2024-05-17 10:08:54 +0300

    rename class

--- src/main/java/org/example/kafka/SimpleKafkaExampleApplication.java
+++ src/main/java/org/example/kafka/SimpleKafkaExampleApplication.java
@@ -2,12 +2,19 @@ package org.example.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
 
+@EnableKafka
 @SpringBootApplication
-public class KafkaApplication {
+public class SimpleKafkaExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaApplication.class, args);
+	@KafkaListener(topics="msg")
+	public void msgListener(String msg){
+		System.out.println(msg);
 	}
 
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleKafkaExampleApplication.class, args);
+	}
 }
