3d7ccdb6372a8f87c44896833b7c573afec595df
AlgosStile <oleg.todor@mail.ru>
2024-05-17 10:11:56 +0300

    refact addres

--- src/main/java/org/example/kafka/controller/KafkaController.java
+++ src/main/java/org/example/kafka/controller/KafkaController.java
@@ -1,30 +1,32 @@
 package org.example.kafka.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("msg")
+@RequestMapping("/api")
 public class KafkaController {
 
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
-    @PostMapping
-    public void sendOrder(String msgId, String msg) {
+    public KafkaController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping("/sendOrder")
+    public void sendOrder(@RequestParam String msgId, @RequestParam String msg) {
         kafkaTemplate.send("msg", msgId, msg);
     }
 
-    @PostMapping
-    public void sendMsg(String msgId, String msg) {
-        ListenableFuture<SendResult<String, String>> future = (ListenableFuture<SendResult<String, String>>) kafkaTemplate.send("msg", msgId, msg);
+    @PostMapping("/sendMsg")
+    public void sendMsg(@RequestParam String msgId, @RequestParam String msg) {
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send("msg", msgId, msg);
         future.addCallback(System.out::println, System.err::println);
         kafkaTemplate.flush();
     }
-
 }
--- src/main/java/org/example/kafka/dto/Address.java
+++ src/main/java/org/example/kafka/dto/Address.java
@@ -2,9 +2,11 @@ package org.example.kafka.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Address {
     private String country;
     private String city;
